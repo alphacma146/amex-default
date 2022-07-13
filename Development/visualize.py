99d09aaad97b675d4328ebf854ecddc54efd67f7
syotaro <m154030kdy@gmail.com>
2022-07-13 23:08:30 +0900

    principal component analysis

--- Development/visualize.py
+++ Development/visualize.py
@@ -1,12 +1,15 @@
 # %%
 # standard lib
+import gc
 from pathlib import Path
 # third party
 import dask.dataframe as dd
 import pandas as pd
 import numpy as np
 from plotly import express as px  # kaleido==0.1.0.post1 (win11)
-from plotly import graph_objects as go
+# from plotly import graph_objects as go
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 # %%
 save_path = Path(r"..\Document\src")
 train_data_path = Path(r"Data\feather_data\train_data.ftr")
@@ -15,6 +18,8 @@ train_labels_path = Path(r"Data\amex-default-prediction\train_labels.csv")
 raw_data = dd.read_csv(Path(r"Data\amex-default-prediction\train_data.csv"))
 print(raw_data.info(), raw_data.size, sep="\n")
 raw_data.head()
+del raw_data
+gc.collect()
 # %%
 train_data = pd.read_feather(train_data_path)
 train_labels = pd.read_csv(train_labels_path)
@@ -48,57 +53,71 @@ fig.show()
 # %%
 
 
-def multiplot(target: int, label: str, data: pd.DataFrame, x_var: pd.Series):
-    ratio = 1
-    obj_list = []
-    stb_list = []
-    fig = go.Figure()
-    for item in data.columns[1:]:
-        if item == "S_2":
-            continue
-        if data[item].dtypes == "object":
-            obj_list.append(item)
-            continue
-        if all(
-            [(ave := data[item].mean()) * (1 - ratio / 100) <=
-             i < ave * (1 + ratio / 100)
-             for i in data[item]]
-        ):
-            stb_list.append(item)
-            continue
-        fig.add_trace(
-            go.Scatter(x=x_var, y=data[item], name=item)
-        )
-    feat_num = len(data.columns[1:]) - len(obj_list) - len(stb_list)
+def correlation_heatmap(data: pd.DataFrame, category) -> None:
+    correlation_matrix = data.corr("spearman")
+    fig = px.imshow(correlation_matrix)
     fig.update_layout(
-        title=f"{label} target={target}, line={feat_num}",
-        xaxis_title="Date",
-        yaxis_title="Normalized var",
-        margin_l=10,
-        margin_b=10,
-        margin_t=30,
-        height=450,
+        # template="plotly_dark",
+        title={
+            "text": f"parameter category {category}",
+            "font": {
+                "size": 22,
+                "color": "black"
+            },
+            "x": 0.5,
+            "y": 0.95,
+        },
+        margin_l=5,
+        margin_b=15,
+        width=700,
+        height=600,
     )
     fig.show()
-    # fig.write_image(save_path / f"transition_{label}_{target}.svg")
-    print(f"{obj_list=}", f"{stb_list=}", sep="\n")
+    fig.write_image(
+        save_path / f"correlation_matrix_{category}.svg", scale=10
+    )
 
 
-for target in train_labels["target"].unique():
-    id_df = train_unique.query(f"target=={target}")["customer_ID"]
-    cid = id_df.values[np.random.randint(0, len(id_df) + 1)]
-    vis_data = train_data.query(f"customer_ID=='{cid}'")
-    for feature, cols in (
-        ("Delinquency", [col for col in train_data.columns if col[0] == "D"]),
-        ("Spend", [col for col in train_data.columns if col[0] == "S"]),
-        ("Payment", [col for col in train_data.columns if col[0] == "P"]),
-        ("Balance", [col for col in train_data.columns if col[0] == "B"]),
-        ("Risk", [col for col in train_data.columns if col[0] == "R"])
-    ):
-        multiplot(
-            target, feature,
-            vis_data[cols],
-            vis_data["S_2"]
-        )
+def principal_component_analysis(data: pd.DataFrame, category: str) -> None:
+    ss = StandardScaler()
+    pca = PCA()
+
+    correlation_heatmap(data, category)
+    st_data = ss.fit_transform(data.fillna(0))
+    pca.fit(st_data)
+    prop_var = np.cumsum(pca.explained_variance_ratio_)
+
+    fig = px.scatter(prop_var)
+    fig.update_layout(
+        # template="plotly_dark",
+        title={
+            "text": f"proportion of variance {category}",
+            "font": {
+                "size": 22,
+                "color": "black"
+            },
+            "x": 0.5,
+            "y": 0.95,
+        },
+        margin_b=15,
+    )
+    fig.show()
+    fig.write_image(
+        save_path / f"prop_var_{category}.svg", scale=10
+    )
+
+
+train_data = (
+    train_data
+    .drop(["S_2", "D_63", "D_64"], axis=1)
+    .set_index("customer_ID")
+)
+col_items = train_data.columns
+for cat in ("R", "D", "S", "P", "B"):
+    data = train_data[[col for col in col_items if col[0] == cat]]
+    principal_component_analysis(data, cat)
+
+    if cat == "R":
+        break
 
 # %%
